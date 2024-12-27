c1052bb6d35a7592f462a7577c25b833d7ba3d51
Myusman <myusman137@gmail.com>
2024-12-27 01:14:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['what i do', 'what i have', 'what they say'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
